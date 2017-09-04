67d4969d9e620ad1fb6b788c6add6b73ed3bc255
Thor Hanson <hansonthor@gmail.com>
2017-09-04 11:16:59 -0500

    Converted site to new template. First test version.

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html>
+
+	<head>
+		
+		    <?php
+				include("formatter.html");
+			?>
+
+			<title>Downloads-Thor Hanson</title>
+	
+	</head>
+	
+	<div class="floatCage1">
+		<div class="floatBox1">
+				<h1 align="center" style="margin-bottom= 0px;">ThorAndrew.com</h1><br>
+				<p style="margin-top: -20px; margin-bottom: 6px;">Thor and Andrew's personal websites redirect here. Click the link you need below.</p>
+		</div>
+	</div>
+	
+	<div class="floatCage2">
+		<div class="floatBox2">
+			<p>
+				<a href="www.thorandrew.com/andrew">Andrew's Site</a>
+			</p>
+			<p class="desc"> 
+				<img src="images/sciPineLogo.png" width="30%">
+			</p>
+		</div>
+		<div class="floatBox2">
+			<p>
+				<a href="/thor/fullFiles/ArchLinuxInstall.pdf" download>Thor's Site</a>
+			</p>
+			<p class="desc"> 
+				Sorry I don't have a fancy picture like Andrew.
+			</p>
+		</div>
+	</div>
+	
+	
+	
+</html>
+
